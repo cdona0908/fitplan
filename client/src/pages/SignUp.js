22736eaca1c58716cefcd055cab48fb5e08ccd9d
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-24 18:48:59 -0400

    setting up function that use a preventDefault that prevents an event for happening and also validates what the users is adding on the input field

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -4,4 +4,25 @@ import {Link} from 'react-router-dom';
 import {useMutation} from '@apollo/client';
 import Auth from '../utils/auth';
 
-//setting up the initi
+//setting up the main signup function
+function Signup(props) {
+    // set the initial form state
+    const [formState, setformState] = useState({email: '', password: ''});
+    const [addUser] = useMutation(ADD_USER);
+
+    //setting up function that use a preventDefault that prevents an event for happening and also validates what the users is adding on the input field
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        const mutationResponse = await addUser({
+            variables: {
+                email: formState.email,
+                password: formState.password,
+                firstName: formState.firstName,
+                lastName: formState.lastName
+            }
+        });
+        const token = mutationResponse.data.addUser.token;
+        Auth.login(token)
+    }
+};
