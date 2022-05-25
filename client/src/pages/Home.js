2568bde91c14c0e8502b6ac807ca87c5f95b0294
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-25 18:28:00 -0400

    creating a function that import the activities from the db

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,29 @@
 import React from "react";
 import {ActivityHome} from '../components/ActivityList/';
 import {useQuery} from '@apollo/client';
-import {Query_Activities} from '../utils/queries';
+import {QUERY_ACTIVITIES} from '../utils/queries';
 import {Wrap} from '@chakra-ui/react';
+
+// creating a function that import the activities from the db
+const Home = () => {
+    const {loading, data: activityData} = useQuery(QUERY_ACTIVITIES);
+
+    if (loading) {
+        return <div>Loading Activities...</div>
+    }
+
+    // Returns the template of Activities
+    return (
+        // <Grid templateColumns='repeat(5, 1fr)' gap{6}>
+        <Wrap spacing='5px' justify='center'>
+            {activityData.activities.map((activity) => {
+                return (
+                    <ActivityHome key={activity.title} activity={activity}></ActivityHome>
+                );
+            })}
+
+        </Wrap>
+        // </Grid>
+    )
+}
 
