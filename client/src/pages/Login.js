6febe7e6264c19dc27524f54112445f22f80b202
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-24 21:25:14 -0400

    setting up function that use a preventDefault that prevents an event for happening and also validates what the users is adding on the input field

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,6 +6,24 @@ import {LOGIN} from '../utils/mutations';
 import Auth from '../utils/auth';
 
 function Login(props) {
+    // set the initial form state
     const [formState, setformState] = useState({email: '', password: ''});
     const [login, {error}] = useMutation(LOGIN);
+
+    //setting up function that use a preventDefault that prevents an event for happening and also validates what the users is adding on the input field
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables: {email: formState.email, password: formState.password},
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (e) {
+            console.log(e)
+        }
+    };
+
+
 }
+
