c9940406b80afa37340781bf9976009ce8769cb7
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-26 16:04:34 -0400

    add adding Importing the parameters that we are going to used to implement the function and also we query a function that loops threw all routines and store the data while the routine are completed

--- client/src/pages/CompletedRoutines.js
+++ client/src/pages/CompletedRoutines.js
@@ -17,5 +17,21 @@ import {
 } from '@chakra-ui/react';
 import { Link } from "react-router-dom";
 import {ArrowBackIcon} from '@chakra-ui/icons';
+const CompletedRoutines = () => {
+    const [reuseRoutine]= useMutation(REUSE_ROUTINE);
 
+    const {data: userData, loading} = useQuery(QUERY_ME);
 
+    const allRoutines = userData?.me.routines;
+
+    if (loading) {
+        return <div>Loading your Routines...</div>
+    }
+
+    let completedRoutines;
+    if(allRoutines) {
+        completedRoutines = allRoutines.filter((routine) => {
+            return routine.isComplete === true;
+        })
+    }
+}
