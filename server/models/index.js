2d4191af40b2eef411e6d540f6f9b7e72716f810
Celia <celiamdona@gmail.com>
2022-05-26 19:07:27 -0400

    edited resolvers and typedef

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const Exercise = require('./Exercise');
-const Routine = require('./Routine');
+//const Routine = require('./Routine');
 const User = require('./User');
 
-module.exports = { Exercise, Routine, User };
+module.exports = { Exercise,  User };
