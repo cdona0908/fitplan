a7281efc925fb54bd254c36276c3b057ab62b48a
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-26 16:32:16 -0400

    Function that returns the routines template and when the routines are incompleted its takes back to dashboard to create new routines

--- client/src/pages/CompletedRoutines.js
+++ client/src/pages/CompletedRoutines.js
@@ -34,4 +34,56 @@ const CompletedRoutines = () => {
             return routine.isComplete === true;
         })
     }
+
+    return (
+        <Box minHeight='83.3vh'>
+            <Heading className='center-text' fontSize='3xl' color ='2C7A7B' mt ='20px'>
+                Completed Routines
+            </Heading>
+            <Center>
+                <Link to='/dashboard'>
+                    <Button leftIcon={<ArrowBackIcon />} variant ='ghost'>
+                        Back to dashboard
+                    </Button>
+                </Link>
+            </Center>
+            {!completedRoutines.length ? (
+                <ScaleFade in>
+                    <Box>
+                        <Alert 
+                            status='info' 
+                            variant='subtle' 
+                            flexDirection='column'
+                            alignItems
+                            justifyContent='center'
+                            textAlign='center'
+                            height='200px'
+                            borderRadius='md'>
+                            <AlertIcon boxSize='40px' mr={0} />
+                            <AlertTitle mt={4} mb={1} fontSize='lg'>
+                                No Routines!
+                            </AlertTitle>
+                            <AlertDescription maxWidth='sm'>
+                                Go back to the dashboard to create new routines.
+                            </AlertDescription>
+                        </Alert>
+                    </Box>
+                </ScaleFade>
+            ) : (
+                completedRoutines.map((routine) => {
+                    return (
+                        <Box key={routine._id} ml='20' mr='20' mb='15'>
+                            <RoutineList 
+                            routine={routine}
+                            completed={true}
+                            reuseRoutine={reuseRoutine}></RoutineList>
+                        </Box>
+                    );
+                })
+                
+            )}
+        </Box>
+    )
 }
+
+export default CompletedRoutines;
