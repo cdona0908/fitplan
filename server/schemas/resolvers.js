c426c9b01b8c542df1bad1f825c05352dd53b392
Celia <celiamdona@gmail.com>
2022-05-26 19:14:05 -0400

    changes to resolvers and typedef

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,16 @@
+const { AuthenticationError } = require('apollo-server-express');
+//const { signToken } = require('../utils/auth');
+const {User, Exercise} = require('../models')
+
+const resolvers = {
+    Query: {
+      
+    },
+  
+    Mutation: {
+      
+    }
+};
+  
+module.exports = resolvers;
+  
