f91e1d90c135aa445d955778283e0a1e85bbafd0
Celia <celiamdona@gmail.com>
2022-05-28 17:15:42 -0400

    username added to routine model

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -6,7 +6,7 @@ const activitySchema = new Schema(
             {
               type: Schema.Types.ObjectId,
               ref: 'Exercise'
-            }
+            }            
         ],
         weight: {
             type: Number
