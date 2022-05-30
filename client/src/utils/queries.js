d5d5bfd0608a67869e90a11a18220ea33ff3e429
Celia <celiamdona@gmail.com>
2022-05-30 14:35:49 -0400

    set up query exercises

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,18 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_EXERCISES = gql`
+    query exercises () {
+        exercises {
+            _id
+            exerciseTitle
+            exerciseDescription
+            image
+            categories {
+                _id
+                categoryName
+            }
+        }
+    }
+`;
+
+
