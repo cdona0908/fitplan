a8c2b2d59acd2e2846cca8a7b2d51fa717f6e227
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-30 15:21:28 -0400

    add changing word activity for exercise

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,25 @@
 //importing parameters that we're going to need to add the homepage for the activities and link it all together to make the function work properly
 import React from "react";
-import {ActivityHome} from '../components/ActivityList/';
+import {ExerciseHome} from '../components/ExerciseList/';
 import {useQuery} from '@apollo/client';
-import {QUERY_ACTIVITIES} from '../utils/queries';
+import {QUERY_EXERCISES} from '../utils/queries';
 import {Wrap} from '@chakra-ui/react';
 
 // creating a function that import the activities from the db
 const Home = () => {
-    const {loading, data: activityData} = useQuery(QUERY_ACTIVITIES);
+    const {loading, data: exerciseData} = useQuery(QUERY_EXERCISES);
 
     if (loading) {
-        return <div>Loading Activities...</div>
+        return <div>Loading Exercises...</div>
     }
 
     // Returns the template of Activities
     return (
         // <Grid templateColumns='repeat(5, 1fr)' gap{6}>
         <Wrap spacing='5px' justify='center'>
-            {activityData.activities.map((activity) => {
+            {exerciseData.exercises.map((exercise) => {
                 return (
-                    <ActivityHome key={activity.title} activity={activity}></ActivityHome>
+                    <ExerciseHome key={exercise.title} exercise={exercise}></ExerciseHome>
                 );
             })}
 
