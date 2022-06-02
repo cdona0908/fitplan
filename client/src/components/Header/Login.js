fc84b096452e43570f4e0e868a8ef3b245121b99
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-06-02 13:54:53 -0400

    updates

--- client/src/components/Header/Login.js
+++ client/src/components/Header/Login.js
@@ -135,7 +135,7 @@ function LoginForm() {
         <Button
           color="#000000"
           variant="ghost"
-          _hover={{ bg: 'teal.300' }}
+          _hover={{ bg: 'gray.400' }}
           onClick={onOpen}>
           Login
         </Button>
@@ -186,7 +186,7 @@ function LoginForm() {
             </ModalBody>
   
             <ModalFooter>
-              <Button onClick={handleFormSubmit} colorScheme="teal" mr={3}>
+              <Button onClick={handleFormSubmit} colorScheme="yellow" mr={3}>
                 Enter
               </Button>
               <Button onClick={onClose}>Cancel</Button>
